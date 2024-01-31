b862023df7277c6f36bba0de8b31502ea983fb80
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-31 19:22:30 +0900

    カウント機能を追加した

--- app.js
+++ app.js
@@ -3,34 +3,54 @@
     document.addEventListener('DOMContentLoaded', function(e) {
         const SWITCH = {
             isSwitch: true,
-            switchText: 'switch',
+            buttonText: 'switch',
+            buttonText: 'count',
+            count: 0,
+            isUpdateCount: function() {
+                let h1Element = document.querySelector('h1');
+                h1Element.textContent = this.count;
+
+            },
             isChangeTextColor: function() {
+                let h1Element = document.querySelector('h1');
                 let pElement = document.querySelector('p');
-                let header = document.querySelector('header');
-                pElement.classList.add('on-switch');
-                header.classList.add('on-switch');
+                    pElement.classList.add('switch-color-one');
+                    h1Element.classList.add('switch-color-one');
+    
             },
             isRevertChangeTextColor: function() {
+                let h1Element = document.querySelector('h1');
                 let pElement = document.querySelector('p');
-                let header = document.querySelector('header');
-                pElement.classList.remove('on-switch');
-                header.classList.remove('on-switch');
+                pElement.classList.remove('switch-color-one');
+                h1Element.classList.remove('switch-color-one');
+            },
+        
+            isCountUp: function() {
+                let h1Element = document.querySelector('h1');
+                ++this.count;
+                h1Element.textContent = this.count;
+                this.isUpdateCount();
             }
         }
-
+        SWITCH.isUpdateCount();
         let pElement = document.querySelector('p');
         pElement.textContent = 'switch:OFF';
         pElement.addEventListener('click', function() {
             if ( SWITCH.isSwitch ) {
-                this.textContent = `${SWITCH.switchText}:ON`;
+                this.textContent = `${SWITCH.buttonText}:ON`;
                 SWITCH.isChangeTextColor();
             } else {
-                this.textContent = `${SWITCH.switchText}:OFF`;
+                this.textContent = `${SWITCH.buttonText}:OFF`;
                 SWITCH.isRevertChangeTextColor();
-                
-                
             }
             SWITCH.isSwitch = !SWITCH.isSwitch;
         },false);
+
+        const btn = document.getElementById('countButton');
+        btn.textContent = SWITCH.buttonText;
+        btn.addEventListener('click', function(){
+            SWITCH.isCountUp();
+        },false);
+    
     },false);
 }
